2d32ca8a8ff92eae6d2c7930780e75945b89a55f
TnS101 <54761133+TnS101@users.noreply.github.com>
2019-09-01 12:29:23 +0300

    Add files via upload

---
+++ C# Advanced Exams/(Demo) C# Advanced Exam - 17 Feb 2019/Socks/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Socks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> leftSocks = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            Queue<int> rightSocks = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            List<int> pairs = new List<int>();
+
+           while (true)
+            {
+                if (leftSocks.Count == 0 || rightSocks.Count ==0)
+                {
+                    break;
+                }
+                int lastLeftSock = leftSocks.Peek();
+                int firstRightSock = rightSocks.Peek();
+
+                if (lastLeftSock > firstRightSock)
+                {
+                    int pair = lastLeftSock + firstRightSock;
+                    pairs.Add(pair);
+                    leftSocks.Pop();
+                    rightSocks.Dequeue();
+                    
+                }
+                else if (lastLeftSock<firstRightSock)
+                {
+                    leftSocks.Pop();
+                }
+                else if (lastLeftSock == firstRightSock)
+                {
+                    rightSocks.Dequeue();
+                    leftSocks.Push(leftSocks.Peek()+1);
+                }
+            }
+            int maxP = pairs.Max();
+            Console.WriteLine(maxP);
+            Console.WriteLine(string.Join(" ",pairs));
+        }
+    }
+}
