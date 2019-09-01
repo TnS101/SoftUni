d292d2df892461888705d89000b621aa2d38fe63
TnS101 <54761133+TnS101@users.noreply.github.com>
2019-09-01 13:11:46 +0300

    Add files via upload

---
+++ C# OOP Exams/C# OOP Retake Exam - 15 August 2019/Astronauts/Models/Mission.cs
@@ -0,0 +1,47 @@
+﻿using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Models.Missions;
+using SpaceStation.Models.Planets;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SpaceStation.Models.Missions
+{
+    public class Mission : IMission
+    {
+        public void Explore(IPlanet planet, ICollection<IAstronaut> astronauts)
+        {
+            IAstronaut astronaut = astronauts.FirstOrDefault(a => a.CanBreath);
+
+            if (planet == null)
+            {
+                return;
+            }
+            if (astronauts == null )
+            {
+                return;
+            }
+            if (planet.Items.Count == 0)
+            {
+                return;
+            }
+
+            while (astronauts.Any(a=>a.CanBreath)&& planet.Items.Count >0)
+            {
+                string item = planet.Items.FirstOrDefault();
+                if (!astronaut.CanBreath)
+                {
+                    astronaut = astronauts.FirstOrDefault(a => a.CanBreath);
+                    continue;
+                }
+                else
+                {
+                    astronaut.Breath();
+                    astronaut.Bag.Items.Add(item);
+                    planet.Items.Remove(item);
+                }
+            }
+        }
+    }
+}
