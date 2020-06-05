70f3739b8b2e19bd092cf6422201bc9ef7161e81
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-06-05 21:06:31 +0300

    Update Solution

--- JS Advanced/Ex DOM/04. Order-the-Names/solution.js
+++ JS Advanced/Ex DOM/04. Order-the-Names/solution.js
@@ -1,17 +1,15 @@
 function solve() {
-    let name = document.getElementsByTagName('input').value;
-    let array = document.getElementsByTagName('ol');
-    let tArray = Array.of(array);
+    document.getElementsByTagName('button')[0].onclick = function exe() {
+        let name = document.getElementsByTagName('input')[0];
 
-    for (let i = 0; i < array.length; i++) {
-        if (array.item(i).value[0] == name[0] && array.item(i).value != null) {
-            array.item(i).value = [array.item(i).value, name].join(', ');
-        } else {
-            tArray.push(name);
-        }
+        let firstLetterCode = (name.value).toUpperCase().charCodeAt(0);
+
+        let a = document.getElementsByTagName('li')[firstLetterCode - 65];
+
+        let nameToInsert = (name.value[0]).toUpperCase() + (name.value).slice(1).toLowerCase();
+
+        (a.textContent === '') ? a.textContent = nameToInsert: a.textContent = a.textContent + ', ' + nameToInsert;
+
+        name.value = '';
     }
-
-    name = '';
-
-    tArray.sort();
 }
