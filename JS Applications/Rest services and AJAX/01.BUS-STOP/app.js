c17e85aad9f588fac6bb9297872410f6b9f50b76
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-07-07 03:02:51 -0700

    Add Solution

--- JS Applications/Rest services and AJAX/01.BUS-STOP/app.js
+++ JS Applications/Rest services and AJAX/01.BUS-STOP/app.js
@@ -1,3 +1,32 @@
 function getInfo() {
-    const button = document.getElementById('submit');
+    const validIds = ['1287', '1308', '1327', '2334'];
+    const inputId = document.getElementById('stopId');
+    const stopName = document.getElementById('stopName');
+    const url = `http://localhost:3000/businfo/${stopId}`;
+    
+    document.getElementById('submit').addEventListener('click',function exe(e)
+    {
+        e.preventDefault();
+        if (validIds.includes(inputId) === false)
+        {
+            stopName.textContent = 'Error';
+            return;
+        }
+
+        fetch(url)
+        .then(result => result.json())
+        .then(data => append(data));
+    });
+
+    function append(data)
+    {
+        stopName.textContent = data.name;
+        data.buses.forEach(el =>
+            {
+                const li = document.createElement('li');
+                li.textContent = `Bus ${el.busId} arrives in ${el.time} minutes`;
+
+                document.getElementById('busses').appendChild(li);
+            });
+    }
 }
