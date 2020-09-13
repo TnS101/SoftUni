81f744cafbadcb8ccb1319faafd46a55ee6a8298
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-09-13 08:38:36 +0300

    Add Application Role

---
+++ C# Web/Cakes/Domain/ApplicationRole.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain
+{
+    using Microsoft.AspNetCore.Identity;
+    using System;
+
+    public class ApplicationRole : IdentityRole
+    {
+        public ApplicationRole(string name)
+            : base(name)
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
